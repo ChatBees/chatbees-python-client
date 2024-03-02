81b8bf5b9f24ff143c43d164e24e82054b15e677
rkang <ryan.kang@nautilusdb.com>
2024-03-01 22:57:03 -0800

    test

--- chatbees/__init__.py
+++ chatbees/__init__.py
@@ -3,5 +3,6 @@ from .client.admin_management import *
 from .client_models.collection import *
 from .client_models.chat import *
 from .server_models.doc_api import *
+from .server_models.ingestion_type import *
 
 from .utils.exceptions import *
